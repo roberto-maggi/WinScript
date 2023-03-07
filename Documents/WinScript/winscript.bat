e6dd2d09b421a7282b116306383cc53aa12114c2
Roberto Maggi <robertodmaggi@gmail.com>
2023-03-07 16:07:46 +0100

    +sw

--- Documents/WinScript/winscript.bat
+++ Documents/WinScript/winscript.bat
@@ -33,3 +33,46 @@ Get-LocalUser | foreach-object {
     $data[4],$data[6],$data[7]
 }  | ?{$_ -notmatch 'DefaultAccount'} | ?{$_ -notmatch 'Guest'} | ?{$_ -notmatch 'SiemensService'} | Out-File -append C:\Users\Administrator\Desktop\report.txt
 
+# Software
+echo "Software" | Out-File -append C:\Users\Administrator\Desktop\report.txt
+echo "----" | Out-File -append C:\Users\Administrator\Desktop\report.txt
+$SoftWares="Web-Server",
+foreach($SW in $SoftWares) 
+{
+    if ((Get-WindowsFeature $SW).InstallState -eq "Installed") {
+        echo "$SW is installed" | Out-File -append C:\Users\Administrator\Desktop\report.txt
+    } 
+    else {
+        echo "$SW is not installed" | Out-File -append C:\Users\Administrator\Desktop\report.txt
+    }
+}
+#  .NET Framework Version
+$release = Get-ItemPropertyValue -LiteralPath 'HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Release
+switch ($release) {
+    { $_ -ge 533320 } { $version = '4.8.1 or later'; break }
+    { $_ -ge 528040 } { $version = '4.8'; break }
+    { $_ -ge 461808 } { $version = '4.7.2'; break }
+    { $_ -ge 461308 } { $version = '4.7.1'; break }
+    { $_ -ge 460798 } { $version = '4.7'; break }
+    { $_ -ge 394802 } { $version = '4.6.2'; break }
+    { $_ -ge 394254 } { $version = '4.6.1'; break }
+    { $_ -ge 393295 } { $version = '4.6'; break }
+    { $_ -ge 379893 } { $version = '4.5.2'; break }
+    { $_ -ge 378675 } { $version = '4.5.1'; break }
+    { $_ -ge 378389 } { $version = '4.5'; break }
+    default { $version = $null; break }
+}
+echo ".NET Framework Version: $version" | Out-File -append C:\Users\Administrator\Desktop\report.txt
+
+# Microsoft .NET Core Windows Server Hosting Bundle
+$DotNETCoreUpdatesPath = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Updates\.NET Core"
+$DotNetCoreItems = Get-Item -ErrorAction Stop -Path $DotNETCoreUpdatesPath
+$NotInstalled = $True
+$DotNetCoreItems.GetSubKeyNames() | Where { $_ -Match "Microsoft .NET Core.*Windows Server Hosting" } | ForEach-Object {
+    $NotInstalled = $False
+    echo "The host has installed $_"   | Out-File -append C:\Users\Administrator\Desktop\report.txt
+}
+$test = test-path -path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Updates\.NET Core"
+if(-not($test)){
+    echo "Can not find ASP.NET Core installed on the host"  | Out-File -append C:\Users\Administrator\Desktop\report.txt
+}
